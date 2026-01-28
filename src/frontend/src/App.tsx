575fc40f24f9e1a93d46d95d8f445963bc11eb81
maolala233 <1613746601@qq.com>
2026-01-28 15:42:34 +0800

    前端新增

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import OntologyCanvas from './components/OntologyCanvas'
+
+function App() {
+    return (
+        <div className="app-container">
+            <OntologyCanvas />
+        </div>
+    )
+}
+
+export default App
